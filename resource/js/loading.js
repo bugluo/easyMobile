9871bdeaafe7d0f4e029900bdb77b5b946026a9e
bugluo <6185763@qq.com>
2014-08-08 19:06:16 +0800

    add many loading

--- resource/js/loading.js
+++ resource/js/loading.js
@@ -1,9 +1,17 @@
 require([
     'module/easyMobile/js/Ecombo_require'
 ], function(E) {
-	var i = 0;
+	var i = 3;
 	var effects = ['bounce','rotateplane','stretch','orbit','roundBounce','win8','win8_linear','ios','facebook','rotation','timer','none'];
-	E.loading('show');
+	E.loading({
+		effect : effects[i],
+		text : '我是一个loading，在等待爱情',
+		bg : 'rgba(255,255,255,0.7)',
+		color : '#000',
+		sizeW : '140',
+		sizeH : '140',
+		container:'body'
+	});
 	// setTimeout(function(){
 	// 	E.loading('hide');
 	// },1500);
